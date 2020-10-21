a6092968880a40af803c7d38dd1aa5f861d07d6a
Hegusung <borrits.guillaume@gmail.com>
2020-10-21 20:13:18 +0200

    portscan: nmap service recon

--- portscan.py
+++ portscan.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import os
 import argparse
 
 from utils.process_inputs import process_inputs, str_comma, str_ports
@@ -10,6 +11,7 @@ def main():
     parser = argparse.ArgumentParser(description='PortScan')
     parser.add_argument('targets', type=str)
     parser.add_argument('-p', metavar='ports', type=str_ports, nargs='?', help='target port', default='80,443', dest='port')
+    parser.add_argument('-sV', action='store_true', help='Service scan (nmap)', dest='service_scan')
     parser.add_argument('--timeout', metavar='timeout', nargs='?', type=int, help='Connect timeout', default=5, dest='timeout')
     # Dispatcher arguments
     parser.add_argument('-w', metavar='number worker', nargs='?', type=int, help='Number of concurent workers', default=10, dest='workers')
@@ -21,15 +23,18 @@ def main():
 
     Output.setup()
 
-    portscan(args.targets, static_inputs, args.workers, args.timeout)
+    portscan(args.targets, static_inputs, args.workers, args.service_scan, args.timeout)
 
     Output.stop()
 
-def portscan(input_targets, static_inputs, workers, timeout):
+def portscan(input_targets, static_inputs, workers, service_scan, timeout):
 
-    args = (timeout,)
+    args = (service_scan, timeout)
 
     dispatch(input_targets, static_inputs, portscan_worker, args, workers=workers)
+
+    # Fix terminal, in case it was broken by nmap
+    os.system("stty echo")
 
 if __name__ == '__main__':
     main()
