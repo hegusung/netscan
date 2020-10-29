e5a40ef71f036f58c976d6de8be1a034a5bf014b
Hegusung <borrits.guillaume@gmail.com>
2020-10-29 18:27:58 +0100

    sshscan: exec command, bruteforce

---
+++ lib/sshscan/ssh_bruteforce.py
@@ -0,0 +1,90 @@
+import copy
+import socket
+import traceback
+import paramiko
+from .ssh import SSH
+from utils.output import Output
+from utils.utils import AuthFailure
+
+def bruteforce_worker(target, timeout):
+    ssh = SSH(target['hostname'], target['port'], timeout)
+
+    for password in target['b_password_list']:
+        username = target['b_username']
+
+        try:
+            success = False
+            stop = False
+
+            success = ssh.auth(username, password)
+            if success:
+                Output.write({'target': ssh.url(), 'message': 'Authentication success with credentials %s and password %s' % (username, password)})
+
+        except paramiko.AuthenticationException as e:
+            pass
+        except ValueError as e:
+            stop = True
+        except paramiko.SSHException as e:
+            Output.write({'target': ssh.url(), 'message': 'Server overloaded, try reducing amount of workers'})
+            stop = True
+        except socket.error:
+            stop = True
+        except Exception as e:
+            stop = True
+            print("%s: %s\n%s" % (type(e), e, traceback.format_exc()))
+
+        if stop:
+            break
+
+    try:
+        sshscan.disconnect()
+    except:
+        pass
+
+
+def bruteforce_generator(target, username_file, password_file, simple_bruteforce=False):
+    password_list = []
+    if password_file != None:
+        password_f = open(password_file)
+        for f in password_f:
+            f = f.strip()
+            password_list.append(f)
+        password_f.close()
+
+    username_f = open(username_file)
+    for u in username_f:
+        u = u.strip()
+        if len(u) == 0:
+            continue
+
+        if not simple_bruteforce:
+            if ':' in u:
+                p = [u.split(':', 1)[-1]]
+                u = u.split(':', 1)[0]
+            else:
+                p = password_list
+        else:
+            if ':' in u:
+                u = u.split(':', 1)[0]
+            p = [u]
+
+        t = copy.copy(target)
+        t['b_username'] = u
+        t['b_password_list'] = p
+
+        yield t
+    username_f.close()
+
+def bruteforce_generator_count(target, username_file, password_file):
+    count = 0
+
+    username_f = open(username_file)
+    for u in username_f:
+        u = u.strip()
+        if len(u) == 0:
+            continue
+
+        count += 1
+    username_f.close()
+
+    return count
---
+++ lib/sshscan/sshscan.py
@@ -0,0 +1,68 @@
+import os.path
+from time import sleep
+import socket
+import traceback
+import struct
+import copy
+import logging
+import paramiko
+
+from .ssh import SSH
+from .ssh_bruteforce import *
+
+from utils.output import Output
+from utils.dispatch import dispatch
+
+logging.getLogger("paramiko").setLevel(logging.CRITICAL)
+
+def sshscan_worker(target, actions, creds, timeout):
+    try:
+
+        ssh = SSH(target['hostname'], target['port'], timeout)
+
+        version = ssh.get_version()
+        if not version:
+            return
+        Output.write({'target': ssh.url(), 'message': '%s' % version})
+
+        if 'username' in creds and 'password' in creds:
+
+            success = ssh.auth(creds['username'], creds['password'])
+            if success:
+                Output.write({'target': ssh.url(), 'message': 'Successful authentication with username %s and password %s' % (creds['username'], creds['password'])})
+
+                if 'command' in actions:
+                    output = "Command '%s':\n" % actions['command']['command']
+                    output += ssh.execute(actions['command']['command'])
+                    Output.write({'target': target['hostname'], 'message': output})
+            else:
+                Output.write({'target': ssh.url(), 'message': 'Authentication failure with username %s and password %s' % (creds['username'], creds['password'])})
+
+        if 'bruteforce' in actions:
+            if 'username_file' in actions['bruteforce'] != None:
+                Output.write({'target': ssh.url(), 'message': 'Starting bruteforce:'})
+
+                username_file = actions['bruteforce']['username_file']
+                password_file = actions['bruteforce']['password_file'] if 'password_file' in actions['bruteforce'] else None
+                bruteforce_workers = actions['bruteforce']['workers']
+
+                # The generator will provide a username:password_list couple
+                gen = bruteforce_generator(target, username_file, password_file)
+                gen_size = bruteforce_generator_count(target, username_file, password_file)
+
+                args = (timeout,)
+                dispatch(gen, gen_size, bruteforce_worker, args, workers=bruteforce_workers, process=False, pg_name=target['hostname'])
+
+    except paramiko.AuthenticationException as e:
+        Output.write({'target': ssh.url(), 'message': 'Authentication failure with username %s and password %s' % (creds['username'], creds['password'])})
+    except ValueError as e:
+        Output.write({'target': ssh.url(), 'message': "Authentication failure because of crypto failure: %s" % str(e)})
+    except paramiko.SSHException as e:
+        pass
+    except socket.error:
+        pass
+    except Exception as e:
+        Output.write({'target': ssh.url(), 'message': '%s: %s\n%s' % (type(e), e, traceback.format_exc())})
+    finally:
+        ssh.disconnect()
+
