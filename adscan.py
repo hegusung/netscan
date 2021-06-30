778f44053e82d6f8ddf7aa13822c1b691b5f53e5
hegusung <hegusung@gmail.com>
2021-07-01 00:00:01 +0200

    List writable GPOs

--- adscan.py
+++ adscan.py
@@ -32,6 +32,7 @@ def main():
     parser.add_argument("--passpol", action='store_true', help='dump password policy from Active Directory')
     parser.add_argument("--trusts", action='store_true', help='dump trusts from Active Directory')
     parser.add_argument("--cacerts", action='store_true', help='List CA certificates from Active Directory')
+    parser.add_argument("--gpos", action='store_true', help='Extract vulnerable GPOS from Active Directory')
     # Bruteforce
     parser.add_argument('--users-brute', metavar='username file', type=str, nargs='?', const='nofile', help='Check the existence of users via TGT request and prits KRB5ASREP hash is Pre-Auth is disable', default=None, dest='users_brute')
     # Dump
@@ -105,6 +106,8 @@ def main():
         actions['trusts'] = {}
     if args.cacerts:
         actions['cacerts'] = {}
+    if args.gpos:
+        actions['gpos'] = {}
     if args.users_brute:
         actions['users_brute'] = {'username_file': args.users_brute}
     if args.gmsa:
