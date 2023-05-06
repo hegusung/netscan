9f36c1866fdf53fe90b532fddcaba3132155351a
hegusung <hegusung@gmail.com>
2023-05-06 16:14:02 +0200

    init_docker_framework added (thanks @jdouliez :) )

---
+++ init_docker_framework.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+#########################
+###       CONFIG      ###
+#########################
+RED="\e[31m"
+GREEN="\e[32m"
+YELLOW="\e[33m"
+BLACK_BACKGROUND="\e[40m"
+BLUE_BACKGROUND="\e[44m"
+ENDCOLOR="\e[0m"
+
+# Exporting all env variables;
+echo -e "${GREEN}[+] Initializing env variables.${ENDCOLOR}"
+export $(grep -v '^#' docker/docker.env | xargs -d '\n')
+
+docker ps -a --filter "name=netscan" --format '{{.Names}}' | grep netscan-kibana > /dev/null
+
+if [ $? -eq 0 ]
+then
+    echo -e "${GREEN}[+] Containers are already built.. Skipping${ENDCOLOR}"
+    echo -e "${GREEN}[+] Starting containers...${ENDCOLOR}"
+    docker-compose start
+    echo -e "${GREEN}[+] Waiting 15 seconds for containers initialization...${ENDCOLOR}"
+    sleep 15
+else
+    echo -e "${GREEN}[+] Configuring the host system to run dockers...${ENDCOLOR}"
+    sudo sysctl -w vm.max_map_count=262144 > /dev/null
+    
+    echo -e "${GREEN}[+] Building & Starting containers...${ENDCOLOR}"
+    docker-compose up --quiet-pull --build --remove-orphans -d > /dev/null
+    docker build . -f docker/Dockerfile -t netscan:latest > /dev/null
+    
+    echo -e "${GREEN}[+] Waiting 60 seconds for containers initialization...${ENDCOLOR}"
+    sleep 60
+    
+    echo -e "${GREEN}[+] Configuring Kibana...${ENDCOLOR}"
+    curl -u elastic:${ELASTIC_PASSWORD} -s -X POST 'http://127.0.0.1:5601/api/saved_objects/_import?createNewCopies=true' -H "kbn-xsrf: true" --form "file=@$(pwd)/kibana/kibana_dashboards.ndjson" > /dev/null
+fi
+
+echo -e "${GREEN}[+] Checking that everything is ok...${ENDCOLOR}"
+if [ $(docker ps  --format '{{.Names}}' | grep netscan-setup | wc -l) -gt 0 ]
+then
+    echo -e "${RED}[!] Oops, something went wrong...${ENDCOLOR}"
+    echo -e "${RED}    You should start investigating by looking at the \"netscan-setup\" container logs...${ENDCOLOR}"
+    exit 1
+else
+    echo -e "${GREEN}[+] Kibana credentials: ${ENDCOLOR}${RED}${BLACK_BACKGROUND} elastic:${ELASTIC_PASSWORD} ${ENDCOLOR}"
+    echo -e "${GREEN}[+] Remember to change the neo4j password at http://localhost:7474/ (default: ${ENDCOLOR}${RED}${BLACK_BACKGROUND} neo4j:neo4j ${ENDCOLOR}${GREEN})${ENDCOLOR}"
+    
+    echo -e "${GREEN}[+] Adding an alias in your personal settings...${ENDCOLOR}"
+    
+    netscan_docker_command="docker run -it --rm --name netscan-tool --env-file $(pwd)/docker/docker.env --network \"netscan-network\" -v \"/:/host\" -v \"$(pwd)/config.cfg:/app/config.cfg\" -h netscan -v \"$(pwd)/logs:/app/logs\" -v \"$(pwd)/server/files:/app/server/files\" -p \"127.0.0.1:445:445\" -p \"127.0.0.1:3890:3890\" -p \"127.0.0.1:8000:8000\" -e HOST_PWD=\$(pwd) -u \$(id -u \${USER}):\$(id -g \${USER}) netscan:latest"
+    netscan_alias_command="alias netscan='${netscan_docker_command}'"
+    
+    if [ -f "$HOME/.zshrc" ]; then
+        sed -i '/^alias netscan/d' ~/.zshrc
+        echo $netscan_alias_command >> ~/.zshrc
+    fi
+    
+    if [ -f "$HOME.bash_aliases" ]; then
+        sed -i '/^alias netscan/d' ~/.bash_aliases
+        echo $netscan_alias_command >> ~/.bash_aliases
+        elif [ -f "$HOME/.bashrc" ]; then
+        sed -i '/^alias netscan/d' ~/.bashrc
+        echo $netscan_alias_command >> ~/.bashrc
+    fi
+    
+    if [ ! -f "$HOME/.zshrc" ] && [ ! -f "$HOME.bash_aliases" ] && [ ! -f "$HOME/.bashrc" ]; then
+        echo -e "${YELLOW}[!] Error. You should set the alias manually ($netscan_alias_command)${ENDCOLOR}"
+    fi
+    
+    echo -e "${GREEN}[+] You can now use the ${ENDCOLOR}${YELLOW}${RED_BACKGROUND} netscan ${ENDCOLOR}${GREEN} command!${ENDCOLOR}"
+    
+    # Launching new shell to get the alias working
+    $SHELL
+fi
